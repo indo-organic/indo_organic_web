b443f4ca825be9316fd233943f6cbf16189121ca
indo_organic <itindoogranic2000@gmail.com>
2024-03-15 14:45:22 +0530

    nab

--- src/header/Navbar.jsx
+++ src/header/Navbar.jsx
@@ -1,39 +1,4 @@
-// import '../styles/Navbar.scss'
-// import logo from '../assets/logo.jpg'
-// import { Link } from 'react-router-dom'
-// const Navbar = () => {
-//   return (
-//     <>
-//     <div className="navbar">
-//       <div className="nav_left">
-//         <img src={logo} alt="" />
-//       </div>
-
-//       <div className="nav_right">
-
-//           < Link to='/' className='nav_link'> home </Link>
-//           < Link to='/ourJourney' className='nav_link'> our journey </Link>
-//           < Link to='/category' className='nav_link'> Category</Link>
-//           < Link to='/researchDev' className='nav_link'> R & D </Link>
-//           < Link to='/career' className='nav_link'> career </Link>
-//           < Link to='/contact' className='nav_link'> Contact us</Link>
-
-
-//       </div>
-
-
-//     </div>
-
-
-
-
-
-
-//     </>
-//   )
-// }
-
-// export default Navbar
+import { CiMenuFries } from "react-icons/ci";
 
 
 // Navbar.js
@@ -51,14 +16,14 @@ const Navbar = () => {
 
   return (
     <nav className={`navbar ${isOpen ? 'open' : ''}`}>
+      
       <div className="navbar-brand">
         <div className="nav_left">
           <img src={logo} alt="" />
         </div>
         <button className="navbar-toggle" onClick={toggleNavbar}>
-          <span>-</span>
-          <span>-</span>
-          <span>-</span>
+        <CiMenuFries />
+
         </button>
       </div>
       <ul className="nav_right">
@@ -70,6 +35,7 @@ const Navbar = () => {
         < Link to='/contact' className='nav_link'> Contact us</Link>
 
       </ul>
+     
     </nav>
   );
 };
