7afe061cf6b343edf75b8fdf4c15748f303cafe9
indo_organic <itindoogranic2000@gmail.com>
2024-03-15 16:04:08 +0530

    terms and conditions

--- src/footer/TermsConditions.jsx
+++ src/footer/TermsConditions.jsx
@@ -1,8 +1,109 @@
-import React from 'react'
-
+import "../styles/TermsCondition.scss"
 const TermsConditions = () => {
   return (
-    <div>TermsConditions</div>
+    <>
+      <div className="termsConditions">
+
+        <div className="t_c_t">Terms and Conditions</div>
+
+
+        <div className="termsconditions_text">
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Introduction</p>
+            <p>These terms and conditions govern your use of this website; by using this website, you accept these terms and conditions in full. If you disagree with these terms and conditions or any part of these terms and conditions, you must not use this website.</p>
+
+          </div>
+
+
+          <div className="t_c_box">
+            <p className="t_c_heading"> License to Use Website</p>
+            <p>Unless otherwise stated, we own the intellectual property rights in the website and material on the website. Subject to the license below, all these intellectual property rights are reserved. You may view, download for caching purposes only, and print pages from the website for your own personal use, subject to the restrictions set out below and elsewhere in these terms and conditions.</p>
+
+          </div>
+
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Acceptable Use</p>
+            <p>You must not use this website in any way that causes, or may cause, damage to the website or impairment of the availability or accessibility of the website; or in any way which is unlawful, illegal, fraudulent, or harmful, or in connection with any unlawful, illegal, fraudulent, or harmful purpose or activity.
+
+            </p>
+
+          </div>
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Restricted Access</p>
+            <p>Access to specific sections of this website is limited. We retain the authority to restrict access to certain areas or the entirety of the site at our discretion. This measure ensures the security and integrity of our platform, safeguarding sensitive information and maintaining a safe browsing environment for our users. By implementing restricted access, we aim to prevent unauthorized use or entry to areas that may contain confidential data or proprietary content. Additionally, such restrictions may be imposed to comply with legal requirements, protect intellectual property rights, or mitigate potential risks associated with unauthorized access. We prioritize the protection of user privacy and the security of our website, striving to maintain a transparent and trustworthy online environment. Users are expected to adhere to our terms and conditions and respect any access limitations set forth by our administration. Should any questions or concerns arise regarding access restrictions, users are encouraged to reach out to our support team for clarification and assistance.
+
+            </p>
+
+          </div>
+
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Limited Warranties</p>
+            <p>We do not warrant the completeness or accuracy of the information published on this website; nor do we commit to ensuring that the website remains available or that the material on the website is kept up to date.
+
+            </p>
+
+          </div>
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Indemnity</p>
+            <p>You hereby indemnify us and undertake to keep us indemnified against any losses, damages, costs, liabilities, and expenses (including without limitation legal expenses and any amounts paid by us to a third party in settlement of a claim or dispute on the advice of our legal advisers) incurred or suffered by us arising out of any breach by you of any provision of these terms and conditions, or arising out of any claim that you have breached any provision of these terms and conditions.
+            </p>
+
+          </div>
+
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Variation</p>
+            <p>We may revise these terms and conditions from time to time. Revised terms and conditions will apply to the use of this website from the date of the publication of the revised terms and conditions on this website.
+
+            </p>
+
+          </div>
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Entire Agreement</p>
+            <p>These terms and conditions, together with our privacy policy, constitute the entire agreement between you and us in relation to your use of this website and supersede all previous agreements in respect of your use of this website.
+
+            </p>
+
+          </div>
+
+          <div className="t_c_box">
+            <p className="t_c_heading">Law and Jurisdiction
+            </p>
+            <p>These terms and conditions will be governed by and construed in accordance with [governing law], and any disputes relating to these terms and conditions will be subject to the exclusive jurisdiction of the courts of [jurisdiction].
+
+            </p>
+
+          </div>
+
+           <p>If you have any questions about these terms and conditions, please contact us.</p>
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+
+      </div>
+
+
+
+
+    </>
   )
 }
 
