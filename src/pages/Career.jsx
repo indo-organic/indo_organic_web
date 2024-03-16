e2d63117e7f257157f67063aa80452e59a2c530e
indo_organic <itindoogranic2000@gmail.com>
2024-03-16 15:48:37 +0530

    career pages

--- src/pages/Career.jsx
+++ src/pages/Career.jsx
@@ -1,16 +1,28 @@
 import '../styles/Career.scss'
+
 const Career = () => {
   return (
     <>
-    <div className="career">
-     <div className="career_img">
       
-     </div>
+      <div className="career">
+
+        <div className="career_1">
+        <img src="https://cdn.pixabay.com/photo/2014/02/27/16/10/flowers-276014_640.jpg" alt="" />
+
+        <div className="career_title">
+          Career
+        </div>
+                  
+        </div>
 
 
 
 
-    </div>
+
+
+
+
+      </div>
     
     
     
